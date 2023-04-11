707c4d53a02fd64e10be117780d802bb5e2f3579
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-04-11 16:23:02 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,27 @@
 // Objetos
 const navItems = document.querySelectorAll(".navbar-button-class");
 const pages = {
-  page1: document.getElementById("page1"),
-  page2: document.getElementById("page2"),
-  page3: document.getElementById("page3"),
-  page4: document.getElementById("page4"),
+    page1: document.getElementById("page1"),
+    page2: document.getElementById("page2"),
+    page3: document.getElementById("page3"),
+    page4: document.getElementById("page4"),
 };
 // Esconde todas as páginas e mostra a página 1
 Object.values(pages).forEach((page) => (page.style.display = "none"));
 pages["page1"].style.display = "block";
 // Define o evento de clique para cada botão da barra de navegação
 navItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    // Remove a classe "active" de todos os botões da barra de navegação
-    navItems.forEach((navItem) => navItem.classList.remove("active"));
-    // Adiciona a classe "active" apenas ao botão clicado
-    e.currentTarget.classList.add("active");
-    // Esconde todas as páginas
-    Object.values(pages).forEach((page) => (page.style.display = "none"));
-    // Mostra a página correspondente ao botão clicado
-    const pageToShow = e.currentTarget.dataset.page;
-    pages[pageToShow].style.display = "block";
-  });
+    item.addEventListener("click", (e) => {
+        // Remove a classe "active" de todos os botões da barra de navegação
+        navItems.forEach((navItem) => navItem.classList.remove("active"));
+        // Adiciona a classe "active" apenas ao botão clicado
+        e.currentTarget.classList.add("active");
+        // Esconde todas as páginas
+        Object.values(pages).forEach((page) => (page.style.display = "none"));
+        // Mostra a página correspondente ao botão clicado
+        const pageToShow = e.currentTarget.dataset.page;
+        pages[pageToShow].style.display = "block";
+    });
 });
 
 function initMap() {
@@ -48,33 +48,34 @@ function initMap() {
   if (navigator.geolocation) {
     const options = {
       enableHighAccuracy: true,
-      maximumAge: 0
+      timeout: 10000,
+      maximumAge: 5000
     };
-    navigator.geolocation.getCurrentPosition(
+    const userMarker = new google.maps.Marker({
+      map: map,
+      icon: {
+        url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+      }
+    });
+    navigator.geolocation.watchPosition(
       (position) => {
-        const pos = {
+        const userLocation = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude
+          lng: position.coords.longitude,
         };
-        map.setCenter(pos);
+        userMarker.setPosition(userLocation);
+        map.setCenter(userLocation);
       },
-      () => {
-        console.log("Erro ao acessar a posição atual do usuário.");
+      (error) => {
+        console.log("Error getting location:", error);
       },
       options
     );
   } else {
-    console.log("O navegador não suporta geolocalização.");
+    console.log("Geolocation is not supported by this browser.");
   }
 }
 
-// Carregar a API do Google Maps
-function loadMapScript() {
-  const script = document.createElement("script");
-  script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCIjn7yc2qdAWYWozx6OpupHdsv0yDFDIs&libraries=places&callback=initMap`;
-  script.defer = true;
-  script.async = true;
-  document.body.appendChild(script);
-}
-
-window.onload = loadMapScript;
+window.onload = () => {
+  initMap();
+};
