63181fbde232ffd6c95c424b22c57496120361d2
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-04-11 15:23:54 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ navItems.forEach((item) => {
 function initMap() {
   const center = { lat: -8.0555435, lng: -34.8806205 }; // Coordenadas do centro de São Paulo
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 17,
+    zoom: 16,
     center: center,
     streetViewControl: false, // Remove a opção de Street View
     mapTypeControl: false, // Remove a opção de visualização de satélite
@@ -90,7 +90,7 @@ function showPosition(position) {
   var lng = position.coords.longitude;
   var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: lat, lng: lng},
-      zoom: 16
+      zoom: 18
   });
   var marker = new google.maps.Marker({
       position: {lat: lat, lng: lng},
