2c9973ede7bfb034f14ded1e28c96b250632dfe3
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-04-11 17:11:39 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,31 +80,35 @@ function initMap() {
     if (navigator.permissions) {
       navigator.permissions.query({ name: "geolocation" }).then((result) => {
         if (result.state === "granted") {
-          const options = {
-            enableHighAccuracy: true,
-            timeout: 10000,
-            maximumAge: 5000
-          };
-          const userMarker = new google.maps.Marker({
-            map: map,
-            icon: {
-              url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-            }
-          });
-          navigator.geolocation.watchPosition(
-            (position) => {
-              const userLocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              };
-              userMarker.setPosition(userLocation);
-              map.setCenter(userLocation);
-            },
-            (error) => {
-              console.log("Error getting location:", error);
-            },
-            options
-          );
+          if (navigator.geolocation) {
+            const options = {
+              enableHighAccuracy: true,
+              timeout: 10000,
+              maximumAge: 5000
+            };
+            const userMarker = new google.maps.Marker({
+              map: map,
+              icon: {
+                url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+              }
+            });
+            navigator.geolocation.watchPosition(
+              (position) => {
+                const userLocation = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+                userMarker.setPosition(userLocation);
+                map.setCenter(userLocation);
+              },
+              (error) => {
+                console.log("Error getting location:", error);
+              },
+              options
+            );
+          } else {
+            console.log("Geolocation is not supported by this browser.");
+          }
         } else if (result.state === "prompt") {
           console.log("Geolocation permission is prompted");
           navigator.geolocation.getCurrentPosition(() => {}, () => {}, {});
