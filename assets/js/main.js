b49085a883d6fc30e74b511b1162c8d11c07567d
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-04-13 17:58:07 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,7 +74,7 @@ function initMap() {
   const arrowNextButton = document.querySelector('.arrow-next');
   const arrowBackButton = document.querySelector('.arrow-back');
 
-  const categoryButtons = document.querySelectorAll('.category-button');
+  const categoryButtons = document.querySelectorAll('.page2-point');
 
   arrowNextButton.addEventListener('click', () => {
     currentCategoryIndex++;
@@ -235,6 +235,29 @@ function initMap() {
     console.log("Geolocation is not supported by this browser.");
   }
 }
+
+const categories = ['Tudo', 'Cinema', 'Parque', 'Igreja'];
+let currentCategoryIndex = 0;
+  
+const cardText = document.querySelector('.card-text');
+const arrowNextButton = document.querySelector('.arrow-next');
+const arrowBackButton = document.querySelector('.arrow-back');
+  
+arrowNextButton.addEventListener('click', () => {
+  currentCategoryIndex++;
+  if (currentCategoryIndex >= categories.length) {
+    currentCategoryIndex = 0;
+  }
+  cardText.textContent = categories[currentCategoryIndex];
+});
+  
+arrowBackButton.addEventListener('click', () => {
+  currentCategoryIndex--;
+  if (currentCategoryIndex < 0) {
+    currentCategoryIndex = categories.length - 1;
+  }
+  cardText.textContent = categories[currentCategoryIndex];
+});
 
 window.onload = () => {
   initMap();
