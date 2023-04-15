5f13954897eea723e16e8b5e37734a35d55be512
douglasgsjunior <125422005+douglasgsjunior@users.noreply.github.com>
2023-04-15 08:09:28 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,7 +76,7 @@ function initMap() {
     {
       name: "Rec'N'Play",
       info: "PE, 50030-230",
-      position: { lat: -8.0555435, lng: -34.8806205 },
+      position: { lat: -8.0571438, lng: -34.8712346 },
       category: "Eventos Culturais & Festivais"
     },
     {
